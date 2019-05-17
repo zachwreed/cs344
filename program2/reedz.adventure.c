edcd9d0aee856ad0d519f94407a2548b33aaaf63
zachwreed <reedz@oregonstate.edu>
2019-05-17 08:50:05 -0700

    program 1 and 2 files

---
+++ program2/reedz.adventure.c
@@ -0,0 +1,438 @@
+/***************************************
+ ** Author: Zach Reed
+ ** Description: Build Rooms for Adventure
+ ** Date: 5/8/2019
+ ****************************************/
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <dirent.h>
+#include <time.h>
+#include <pthread.h>
+
+#define ROOMS_LN 10
+#define ROOMS_N 7
+#define ROOMS_T 3
+#define CONNECT_MAX 6
+#define CONNECT_MIN 3
+#define ROOM_NAME_PREFIX 11
+#define CONNECT_PREFIX 11
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+struct room{
+    char* type;
+    char* name;
+    int connect_n;
+    struct room* connections[CONNECT_MAX];
+};
+
+char working_dir[256];
+struct stat* file;
+struct room* rooms[ROOMS_N];
+struct room* g_rooms[100];
+int rn_steps;
+int rooms_idx;                  // used for adding rooms
+char* gtime;
+char* room_names[ROOMS_LN] = { "Eastmarch", "Falkreath", "Haafingar", "Hjaalmarch", "The_Pale", "The_Reach", "The_Rift", "Whiterun", "Winterhold", "Oblivion" };
+char* room_types[ROOMS_T] = { "START_ROOM", "END_ROOM", "MID_ROOM" };
+
+/*****************************************************
+ ** Constructor
+ ** Preconditions:  rooms is NULL
+ ** Postconditions: array is initiallized
+ *****************************************************/
+void constructor() {
+    memset(working_dir, '\0', sizeof(working_dir));
+    
+    rn_steps = 0;
+    int k;
+    for ( k = 0; k < ROOMS_N; k++) {
+        rooms[k] = (struct room *)malloc(sizeof(struct room));
+        rooms[k]->connect_n = 0;
+    }
+}
+
+/*****************************************************
+ ** Deconstructor
+ ** Preconditions:  rooms is not NULL
+ ** Postconditions: rooms is NULL
+ *****************************************************/
+void deconstructor() {
+    int k;
+    for (k = 0; k < ROOMS_N; k++) {
+        free(rooms[k]);
+    }
+}
+
+/*****************************************************
+ ** Is Room File
+ ** Preconditions: read_directory() has been called
+ ** Postconditions: returns true if room name has prefix in room_names
+ *****************************************************/
+int is_room(char *file_name) {
+    int i;
+    for (i = 0; i < ROOMS_LN; i++) {
+        if(strstr(file_name, room_names[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/*****************************************************
+ ** Is Room in Array
+ ** Preconditions: read_directory() has been called
+ ** Postconditions: returns true if room name has prefix in room_names
+ *****************************************************/
+int is_room_arr(char *file_name) {
+    int i;
+    for (i = 0; i < ROOMS_N; i++) {
+        if(strstr(file_name, rooms[i]->name)) {
+            return i;
+        }
+    }
+    return -1;
+}
+/*****************************************************
+ ** Is Room Connected
+ ** Preconditions: con_idx is the index of a room in room_names
+ ** Postconditions: returns true if room name is connection name
+ *****************************************************/
+int is_room_connect(struct room* r, int con_idx) {
+    int i;
+    for (i = 0; i < r->connect_n; i++) {
+        if (r->connections[i]->name == rooms[con_idx]->name) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/*****************************************************
+ ** Is Type
+ ** Preconditions: read_directory() has been called
+ ** Postconditions: returns index if type is in room_types[]
+ *****************************************************/
+int is_type(char *file_name) {
+    int i;
+    file_name += CONNECT_PREFIX;
+    for (i = 0; i < ROOMS_T; i++) {
+        if (strstr(file_name, room_types[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+/*****************************************************
+ ** Get Room
+ ** Preconditions: read_directory() has been called
+ ** Postconditions: returns room of name shared by room_name[rn_idx]
+ *****************************************************/
+struct room* get_room(int rn_idx) {
+    int i;
+    for (i = 0; i < ROOMS_N; i++) {
+        if(rooms[i]->name == room_names[rn_idx]) {
+            return rooms[i];
+        }
+    }
+    return NULL;
+}
+
+/*****************************************************
+ ** Add Room
+ ** Preconditions: is_room() returned true
+ ** Postconditions: room added to room array
+ *****************************************************/
+void add_room(char *file_name, int nidx) {
+    FILE *room = fopen(file_name, "r");
+    rooms[rooms_idx]->name = room_names[nidx];
+    char room_line[100];
+    fgets(room_line, 100, room);
+    while (strstr(fgets(room_line, 100, room), "CONNECTION ")) {
+        rooms[rooms_idx]->connect_n++;
+        memset(room_line, '\0', sizeof(room_line));
+    }
+    
+    // get type from file
+    int tidx = is_type(room_line);
+    if (tidx > -1) {
+        rooms[rooms_idx]->type = room_types[tidx];
+    }
+    
+    fclose(room);
+    rooms_idx++;
+}
+
+/*****************************************************
+ ** Add Room Connections
+ ** Preconditions: is_room() returned true
+ ** Postconditions: room added to room array connections[]
+ *****************************************************/
+void add_room_connections(char *file_name, int nidx, int ridx) {
+    FILE *room = fopen(file_name, "r");
+    char room_line[256];
+    fgets(room_line, 256, room);
+    int rnidx;
+    int cidx = 0;
+    while (strstr(fgets(room_line, 256, room), "CONNECTION ")) {
+        rnidx = is_room(room_line);
+        if (ridx > -1) {
+            rooms[ridx]->connections[cidx] = get_room(rnidx);
+            cidx++;
+        }
+    }
+}
+
+/*****************************************************
+ ** Get Rooms
+ ** Preconditions: get_directory() called
+ ** Postconditions: reads rooms from directory for writing
+ *****************************************************/
+void get_rooms(char* newest_dir) {
+    struct dirent* rooms_in_dir;
+    DIR* rooms_to_check = opendir(newest_dir);
+    char new_file[256];
+    int index;
+    
+    if (rooms_to_check > 0) {
+        while ((rooms_in_dir = readdir(rooms_to_check))) {
+            index = is_room(rooms_in_dir->d_name);
+            if (index != -1) {
+                // clear string and add file path
+                memset(new_file, '\0', sizeof(new_file));
+                strcpy(new_file, newest_dir);
+                strcat(new_file, "/");
+                strcat(new_file, rooms_in_dir->d_name);
+                add_room(new_file, index);
+            }
+        }
+    }
+}
+
+/*****************************************************
+ ** Get Room Connections
+ ** Preconditions: get_directory() called
+ ** Postconditions: reads rooms from directory for writing
+ *****************************************************/
+void get_room_connections(char *newest_dir) {
+    struct dirent* rooms_in_dir;
+    DIR* rooms_to_check = opendir(newest_dir);
+    char new_file[256];
+    int index;
+    int r_idx = 0;
+    if (rooms_to_check > 0) {
+        while ((rooms_in_dir = readdir(rooms_to_check))) {
+            index = is_room(rooms_in_dir->d_name);
+            if (index != -1) {
+                // clear string and add file path
+                memset(new_file, '\0', sizeof(new_file));
+                strcpy(new_file, newest_dir);
+                strcat(new_file, "/");
+                strcat(new_file, rooms_in_dir->d_name);
+                add_room_connections(new_file, index, r_idx);
+                r_idx++;
+            }
+        }
+    }
+}
+
+/*****************************************************
+ ** Get Room Connections
+ ** Preconditions: rooms[] has been generated from file
+ ** Postconditions: initializes game rooms[] with START ROOM
+ *****************************************************/
+void g_room_gen() {
+    int i;
+    for (i = 0; i < ROOMS_N; i++) {
+        // if room is START_ROOM
+        if (rooms[i]->type == room_types[0]) {
+            g_rooms[0] = rooms[i];
+        }
+    }
+}
+
+/*****************************************************
+ ** Get Time
+ ** Preconditions:
+ ** Postconditions: gtime is updated with current time
+ *****************************************************/
+void* get_time() {
+    pthread_mutex_lock(&mutex);
+    
+    //size_t wdlen = sizeof(working_dir);
+    //size_t ctlen = 16;
+    //char* time_file = (char *) malloc(sizeof(char) * (wdlen + ctlen));
+    //sprintf(time_file, "%s/currentTime.txt", working_dir);
+    FILE *file = fopen("currentTime.txt", "r");
+    char line[256];
+    char line_cpy[256];
+    memset(line, '\0', 256);
+    memset(line_cpy, '\0', 256);
+    if (file) {
+        while (fgets(line, 256, file)) {
+            strcpy(line_cpy, line);
+        }
+        printf("\n%s", line_cpy);
+        
+    }
+    fclose(file);
+    pthread_mutex_unlock(&mutex);
+}
+
+/*****************************************************
+ ** Write Time
+ ** Preconditions: none
+ ** Postconditions: currentTime.txt in . directory file appended with time if exists
+ *****************************************************/
+void write_time() {
+    
+    pthread_mutex_lock(&mutex);
+    
+    char str[80];
+    size_t maxsize = 256;
+    time_t rawtime;
+    const struct tm *time_p;
+    
+    time(&rawtime);
+    time_p = localtime(&rawtime);
+    
+    strftime(str, maxsize, "%I:%M%p, %A, %B %d, %Y\n", time_p);
+    FILE *file = fopen("currentTime.txt", "a");
+    fputs(str, file);
+    fclose(file);
+    
+    pthread_mutex_unlock(&mutex);
+}
+
+
+/*****************************************************
+ ** Play Game
+ ** Preconditions: get_rooms() and get_connections() has been called
+ ** Postconditions: game exits
+ *****************************************************/
+void play_game() {
+    
+    // Initialize getline variables
+    char *line;
+    size_t line_size = 256;
+    size_t buff_char;
+    line = (char *)malloc(sizeof(char) * line_size);
+    
+    // prepare game array
+    g_room_gen();
+    struct room* current_room;
+    current_room = g_rooms[0];
+    int idx = 1;
+    int gbool = 0;
+    
+    // while game is being played ---------------------------------------------
+    while(gbool == 0) {
+        // if current room is end room ----------------------------
+        if(current_room->type == room_types[1]) {
+            printf("YOU HAVE FOUND THE END ROOM. CONGRATULATIONS!\n");
+            printf("YOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", rn_steps);
+            int j;
+            for (j = 1; j < rn_steps; j++) {
+                printf("%s\n", g_rooms[j]->name);
+            }
+            printf("%s\n", current_room->name);
+            gbool = 1;
+        }
+        else {
+            // print current room name ----------------------------
+            printf("\nCURRENT LOCATION: %s\n", current_room->name);
+            int i;
+            // print connections ----------------------------------
+             printf("POSSIBLE CONNECTIONS: ");
+            for (i = 0; i < (current_room->connect_n - 1); i++) {
+                printf("%s, ", current_room->connections[i]->name);
+            }
+            printf("%s.", current_room->connections[i++]->name);
+            
+            // Where To? loop -------------------------------------
+            while(1) {
+                printf("\nWHERE TO? >");
+                buff_char = getline(&line, &line_size, stdin);
+                
+                int is_r = is_room_arr(line);
+                if (is_r != -1) {
+                    int is_rc = is_room_connect(current_room, is_r);
+                    
+                    if(is_rc != -1) {
+                        current_room = rooms[is_r];
+                        g_rooms[idx] = current_room;
+                        idx++;
+                        rn_steps++;
+                        break;
+                    }
+                    else {
+                        printf("\nHUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
+                        break;
+                    }
+                }
+                else {
+                    if (strstr(line, "time")) {
+                        write_time();
+                        pthread_t thread;
+                        pthread_mutex_lock(&mutex);
+                        int res = pthread_create(&thread, NULL, get_time, NULL);
+                        pthread_mutex_unlock(&mutex);
+                        res = pthread_join(thread, NULL);
+                    }
+                    else {
+                        printf("\nHUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    free(line);
+}
+
+int main() {
+    constructor();
+    rooms_idx = 0;
+    
+    char prefix_dir[32] = "reedz.rooms.";
+    char newest_dir[256];
+    int newest_dir_time = -1;
+    memset(newest_dir, '\0', sizeof(newest_dir));
+    DIR* dir_to_check;
+    struct dirent* file_in_dir;
+    struct stat dir_attr;
+    
+    dir_to_check = opendir(".");
+    
+    // if directory opens
+    if (dir_to_check > 0) {
+        // while file in directory
+        while ((file_in_dir = readdir(dir_to_check)) != NULL) {
+            // if file name = prefix directory characters
+            if(strstr(file_in_dir->d_name, prefix_dir)) {
+                // get directory attributes
+                stat(file_in_dir->d_name, &dir_attr);
+                
+                // if directory time is newer than stored time
+                if ((int)dir_attr.st_mtime > newest_dir_time) {
+                    newest_dir_time = (int)dir_attr.st_mtime;
+                    memset(newest_dir, '\0', sizeof(newest_dir));
+                    strcpy(newest_dir, file_in_dir->d_name);
+                }
+            }
+        }
+        strcpy(working_dir, newest_dir);
+        get_rooms(newest_dir);
+        get_room_connections(newest_dir);
+        play_game();
+    }
+    deconstructor();
+    return 0;
+}
---
+++ program2/reedz.buildrooms.c
@@ -0,0 +1,266 @@
+/***************************************
+ ** Author: Zach Reed
+ ** Description: Build Rooms for Adventure
+ ** Date: 5/8/2019
+ ****************************************/
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define ROOMS_LN 10
+#define ROOMS_N 7
+#define ROOMS_T 3
+#define CONNECT_MAX 6
+#define CONNECT_MIN 3
+
+struct room{
+    char* type;
+    char* name;
+    int connect_n;
+    struct room* connections[CONNECT_MAX];
+};
+
+
+struct room* rooms[ROOMS_N];
+int room_bool_n[ROOMS_LN];
+int room_bool_t[ROOMS_T];
+char* room_names[ROOMS_LN] = { "Eastmarch", "Falkreath", "Haafingar", "Hjaalmarch", "The_Pale", "The_Reach", "The_Rift", "Whiterun", "Winterhold", "Oblivion" };
+char* room_types[ROOMS_T] = { "START_ROOM", "END_ROOM", "MID_ROOM" };
+char dir_name[11] = "reedz.rooms";  // name of directory without pID
+char* dir_str;                      // stores dir_name + pID
+int dir_str_len = 12;               // initial length, will be updated with pID
+
+
+/*****************************************************
+ ** Generate Initializer
+ ** Preconditions: room_bool_t, room_bool_n, and rooms are NULL
+ ** Postconditions: all arrays are initialized
+ *****************************************************/
+void gen_initializer() {
+    // initialize room bool to false
+    int i; for ( i = 0; i < ROOMS_T; i++) {
+        room_bool_t[i] = -1;
+    }
+    
+    // initialize room bool to false
+    int j; for ( j = 0; j < ROOMS_LN; j++) {
+        room_bool_n[j] = 0;
+    }
+    
+    // initialize room array with empty room
+    
+    int k; for ( k = 0; k < ROOMS_N; k++) {
+        rooms[k] = (struct room *)malloc(sizeof(struct room));
+        rooms[k]->connect_n = 0;
+    }
+    
+}
+
+/*****************************************************
+ ** Make Room File
+ ** Preconditions: dir_str = name + pID, dir_str_len = 11 + pID
+ ** Postconditions: file has been written with information.
+ *****************************************************/
+void make_room_file(struct room* roomf) {
+    // allocate space for file name
+    int room_name_len = strlen(roomf->name);
+    room_name_len += 5; // for "_room"
+    char* room_dir = (char *)malloc(sizeof(char) * (dir_str_len + room_name_len));
+    
+    // copy directory and name for file
+    strcpy(room_dir, dir_str);
+    strcat(room_dir, "/");
+    strcat(room_dir, roomf->name);
+    strcat(room_dir, "_room");
+    
+    // write file room contents
+    FILE *room_file = fopen(room_dir, "w+");
+    fprintf(room_file, "ROOM NAME: %s\n", roomf->name);
+    int i; for ( i = 0; i < roomf->connect_n; i++) {
+        fprintf(room_file, "CONNECTION %d: %s\n", i + 1, roomf->connections[i]->name);
+    }
+    fprintf(room_file, "ROOM TYPE: %s\n", roomf->type);	
+    fclose(room_file);
+}
+
+/*****************************************************
+ ** Make Directory
+ ** Preconditions: dir_str = null, dir_str_len = 11, no other functions are called in
+    main before make_dir()
+ ** Postconditions: dir_str = reedz.rooms. + pID, dir_str_len += pID length
+ *****************************************************/
+void make_dir() {
+    int pid = getpid();
+    int pid_len = 5;
+    if (pid > 9999) {
+        pid_len = 6;
+    }
+    // Put pID in string
+    char* pid_str = (char *)malloc(sizeof(char) * pid_len);
+    sprintf(pid_str, ".%d", pid);
+    
+    dir_str = (char *)malloc(sizeof(char) * (dir_str_len + pid_len));
+    strcat(dir_str, dir_name);
+    strcat(dir_str, pid_str);
+    dir_str_len += pid_len;
+    int result = mkdir(dir_str, 0755);
+}
+
+/*****************************************************
+ ** Generate Room Names
+ ** Preconditions: room_bool_n is uninitialized
+ ** Postconditions: rooms->names added, room_bool_n[0-max] = 1
+ *****************************************************/
+void gen_room_names() {
+    
+    // Generate rooms with names
+    int idx = 0;
+    int r;
+    while (idx < ROOMS_N) {
+        r = rand() % ROOMS_LN;
+        
+        // if random room name hasn't been used
+        if (room_bool_n[r] == 0) {
+            rooms[idx]->name = room_names[r];
+            room_bool_n[r] = 1;
+            idx++;
+        }
+    }
+}
+
+/*****************************************************
+ ** Generate Room Types
+ ** Preconditions: room_bool_n is uninitialized
+ ** Postconditions: rooms->names added, room_bool_n[0-max] = 1
+ *****************************************************/
+void gen_room_types() {
+    
+    // Generate rooms with types
+    int idx = 0;
+    int start;
+    int end;
+    // Generate Start Module
+    start = rand() % ROOMS_N;
+    rooms[start]->type = room_types[0];
+    
+    // Generate End Module
+    end = rand() % ROOMS_N;
+    while(1) {
+        if (end != start) {
+            rooms[end]->type = room_types[1];
+            break;
+        }
+        end = rand() % ROOMS_N;
+    }
+    
+    // Generate rest of Middle Modules
+    while (idx < ROOMS_N) {
+        if (idx != start && idx != end) {
+            rooms[idx]->type = room_types[2];
+        }
+        idx++;
+    }
+    
+}
+
+/*****************************************************
+ ** Is Graph Full
+ ** Preconditions:
+ ** Postconditions: returns 0 or 1 if there are at least 3
+    outbound connections to each room
+ *****************************************************/
+int is_graph_full() {
+    int i; for ( i = 0; i < ROOMS_N; i++) {
+        if(rooms[i]->connect_n < CONNECT_MIN) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+/*****************************************************
+ ** Connection already exists
+ ** Preconditions: Rooms a and b are constructed
+ ** Postconditions: returns 1 if other room is in
+    room connection array
+ *****************************************************/
+int connect_already_exists(struct room* a, struct room* b) {
+    int i; for ( i = 0; i < CONNECT_MAX; i++) {
+        if (a->connections[i] == b || b->connections[i] == a) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/*****************************************************
+ ** Connect Rooms
+ ** Preconditions: a and b are constructed and not already connected
+ ** Postconditions: a and b added to each room connection array
+ *****************************************************/
+void connect_rooms(struct room* a, struct room* b) {
+    int a_idx = a->connect_n;
+    int b_idx = b->connect_n;
+    a->connections[a_idx] = b;
+    b->connections[b_idx] = a;
+    a->connect_n++;
+    b->connect_n++;
+}
+
+/*****************************************************
+ ** Generate Room Connections
+ ** Preconditions: graph is not already full
+ ** Postconditions: graph is full
+ *****************************************************/
+void gen_room_connections() {
+    int rc1, rc2;
+    while(is_graph_full() == 0) {
+        rc1 = rand() % ROOMS_N;
+        rc2 = rand() % ROOMS_N;
+        
+        // if not same index --------------------
+        if (rc1 != rc2) {
+            // if room connect 1 in range -------
+            if (rooms[rc1]->connect_n < CONNECT_MAX) {
+                //if room connect 2 in range ----
+                if (rooms[rc2]->connect_n < CONNECT_MAX) {
+                    if ((connect_already_exists(rooms[rc1], rooms[rc2])) == 0) {
+                        connect_rooms(rooms[rc1], rooms[rc2]);
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+/*****************************************************
+ ** Generate Room Files
+ ** Preconditions: room name, type, and connections have been generated
+ ** Postconditions: files have been added to reedz.rooms.pid
+ *****************************************************/
+void gen_room_files() {
+    int i; for ( i = 0; i < ROOMS_N; i++) {
+        make_room_file(rooms[i]);
+    }
+}
+    
+int main() {
+    // seed random generator
+    time_t t;
+    srand((unsigned)time(&t));
+    
+    // make directory and files -----------
+    make_dir();
+    gen_initializer();
+    gen_room_names();
+    gen_room_types();
+    gen_room_connections();
+    gen_room_files();
+    return 0;
+}
