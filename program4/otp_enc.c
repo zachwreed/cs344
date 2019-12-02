1824f09800602d37a535490425bbd50a711ddc87
Zach Reed <zacwreed@gmail.com>
2019-12-02 07:55:51 -0600

    Update

--- program4/otp_enc.c
+++ program4/otp_enc.c
@@ -6,52 +6,148 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+// Boolean
+#define TRUE 1
+#define FALSE 0
 
-void error(const char *msg) { perror(msg); exit(0); } // Error function used for reporting issues
+// Error function used for reporting issues
+// change to error(msg, exit status) later -------------------------------------
+void error(const char *msg) {
+	perror(msg);
+	exit(0);
+}
 
-int main(int argc, char *argv[])
-{
-	int socketFD, portNumber, charsWritten, charsRead;
-	struct sockaddr_in serverAddress;
-	struct hostent* serverHostInfo;
-	char buffer[256];
+int main(int argc, char *argv[]) {
+	int socketFD;				// socket file descriptor
+	int portNumber;			// port of otp_enc_d process
+	int charsWritten;		// char[] size sent to otp_enc_d process
+	int charsRead;			// char[] size received from otp_enc_d response
+	int argValid;				// return from check_args
 
-	if (argc < 3) { fprintf(stderr,"USAGE: %s hostname port\n", argv[0]); exit(0); } // Check usage & args
+	int isBG = FALSE;		// is background
+	int isRd = FALSE;		// is file redirect
+	int isRdIdx = -1;		// index of argv[] where file will be redirected
+	int outFile = -1; 	// holds stdout redirect file
+	int outDup = -1;		// holds dup2 return
 
-	// Set up the server address struct
+	FILE *keyF = NULL; 	// holds stdin key file
+	FILE *textF = NULL; // holds stdin text fileIn
+
+
+	struct sockaddr_in serverAddress;		// Holds address of otp_enc_d process plus settings
+	struct hostent* serverHostInfo;			// Holds hostname from args
+	char buffer[256];										// char array used for receiving from otp_enc_d
+
+	/******************************
+	** Check Args
+	*******************************/
+	if (argc < 4 || argc > 7) {
+		fprintf(stderr,"input contains bad characters\n");
+		exit(0);
+	}
+
+	// Loop and check arguments for redirection
+	int i;
+	for (i = 0; i < argc; i++) {
+		if (strcmp(argv[i], ">") == 0) {
+			// if redirection is in range
+			if (i != (argc - 1) && i != 0) {
+				error("input contains bad characters");
+			}
+			else {
+				isRd = TRUE;
+				isRdIdx = i + 1;
+			}
+		}
+	}
+
+	// If stdout redirection
+	if (isRd == TRUE) {
+		// Open outFile from argv
+		outFile = open(argv[isRdIdx], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+		if (outFile < 0) {
+			error(argv[isRdIdx]);
+		}
+		// Redirect stdout to outFile
+		outDup = dup2(outFile, 0);
+		if (outDup < 0) {
+			error("open error");
+		}
+	}
+
+	// Open Stdin File
+	textF = fopen(argv[1], "r");
+	memset(buffer, '\0', sizeof(buffer)); 		// Clear out the buffer array
+	fgets(buffer, sizeof(buffer) - 1, textF); // Get input from the user, trunc to buffer - 1 chars, leaving \0
+	buffer[strcspn(buffer, "\n")] = '\0'; 		// Remove the trailing \n that fgets adds
+
+
 	memset((char*)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	portNumber = atoi(argv[2]); // Get the port number, convert to an integer from a string
-	serverAddress.sin_family = AF_INET; // Create a network-capable socket
-	serverAddress.sin_port = htons(portNumber); // Store the port number
-	serverHostInfo = gethostbyname(argv[1]); // Convert the machine name into a special form of address
-	if (serverHostInfo == NULL) { fprintf(stderr, "CLIENT: ERROR, no such host\n"); exit(0); }
-	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length); // Copy in the address
+	portNumber = atoi(argv[3]); 									// Get the port number, convert to an integer
+	serverHostInfo = gethostbyname("localhost"); 	// Convert the machine name into a type of address
+	serverAddress.sin_family = AF_INET; 					// Create a network-capable socket
+	serverAddress.sin_port = htons(portNumber);		// Store the port number
 
-	// Set up the socket
+	if (serverHostInfo == NULL) {
+		fprintf(stderr, "CLIENT: ERROR, no such host\n");
+		exit(0);
+
+	}
+	 // Copy in the address memcpy(destination, source, size_t)
+	memcpy((char*)&serverAddress.sin_addr.s_addr, (char*)serverHostInfo->h_addr, serverHostInfo->h_length);
+
+	/***************************
+	**  Set up the socket
+	** int <file descriptor> = socket(int domain, int type, in protocol)
+	** domain = AF_INET for cross-network, AF_UNIX for same-machine
+ 	** type = UPD or TCP
+	****************************/
 	socketFD = socket(AF_INET, SOCK_STREAM, 0); // Create the socket
 	if (socketFD < 0) error("CLIENT: ERROR opening socket");
 
-	// Connect to server, cast sockaddir 
-	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to address
+	/***************************
+	**  Connect socket to server address
+	** int <0 success, -1 failure> connect(int sockedFD, cast otp_enc_d &address, size_t otp_enc_d address)
+	** domain = AF_INET for cross-network, AF_UNIX for same-machine
+	** type = UPD or TCP
+	****************************/
+	if (connect(socketFD, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
 		error("CLIENT: ERROR connecting");
+	}
 
-	// Get input message from user
-	printf("CLIENT: Enter text to send to the server, and then hit enter: ");
-	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer array
-	fgets(buffer, sizeof(buffer) - 1, stdin); // Get input from the user, trunc to buffer - 1 chars, leaving \0
-	buffer[strcspn(buffer, "\n")] = '\0'; // Remove the trailing \n that fgets adds
+	/***************************
+	** Send Data to otp_enc_d process
+	** size_t <bytes sent> send(int sockedFD, message, message size_t, int flags)
+	****************************/
+	charsWritten = send(socketFD, buffer, strlen(buffer), 0); 	// Write to the server
+	if (charsWritten < 0) {
+		error("CLIENT: ERROR writing to socket");
+	}
 
-	// Send message to server
-	charsWritten = send(socketFD, buffer, strlen(buffer), 0); // Write to the server
-	if (charsWritten < 0) error("CLIENT: ERROR writing to socket");
-	if (charsWritten < strlen(buffer)) printf("CLIENT: WARNING: Not all data written to socket!\n");
+	if (charsWritten < strlen(buffer)) {
+		printf("CLIENT: WARNING: Not all data written to socket!\n");
+	}
 
 	// Get return message from server
 	memset(buffer, '\0', sizeof(buffer)); // Clear out the buffer again for reuse
-	charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data from the socket, leaving \0 at end
-	if (charsRead < 0) error("CLIENT: ERROR reading from socket");
+
+	/***************************
+	** Receive Data to otp_enc_d process
+	** size_t <bytes received> recv(int sockedFD, message, message size_t, int flags)
+	****************************/
+	charsRead = recv(socketFD, buffer, sizeof(buffer) - 1, 0); // Read data leaving \0 at end
+	if (charsRead < 0) {
+		error("CLIENT: ERROR reading from socket");
+	}
+
 	printf("CLIENT: I received this from the server: \"%s\"\n", buffer);
 
-	close(socketFD); // Close the socket
+	close(socketFD);	// Close the socket
+	// Close Stdout File if Redirect
+	if (isRd == TRUE) {
+		close(outFile);
+	}
 	return 0;
 }
--- program4/otp_enc_d.c
+++ program4/otp_enc_d.c
@@ -5,22 +5,40 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
 
-void error(const char *msg) { perror(msg); exit(1); } // Error function used for reporting issues
+// Error function used for reporting issues
+void error(const char *msg) {
+	perror(msg);
+	exit(1);
+}
 
 int main(int argc, char *argv[])
 {
-	int listenSocketFD, establishedConnectionFD, portNumber, charsRead;
-	socklen_t sizeOfClientInfo;
-	char buffer[256];
-	struct sockaddr_in serverAddress, clientAddress;
+	int listenSocketFD;
+	int establishedConnectionFD;
+	int portNumber;
+	int charsRead;
+	
+	pid_t spawnPid = -5;
+	int sp_child_exit = -5;
 
-	if (argc < 2) { fprintf(stderr,"USAGE: %s port\n", argv[0]); exit(1); } // Check usage & args
+	char buffer[256];
+	socklen_t sizeOfClientInfo;
+	struct sockaddr_in serverAddress;
+	struct sockaddr_in clientAddress;
+
+	if (argc < 2) {
+		fprintf(stderr,"USAGE: %s port\n", argv[0]);
+		exit(1);
+	} // Check usage & args
 
 	// Set up the address struct for this process (the server)
 	memset((char *)&serverAddress, '\0', sizeof(serverAddress)); // Clear out the address struct
-	portNumber = atoi(argv[1]); // Get the port number, convert to an integer from a string
-	serverAddress.sin_family = AF_INET; // Create a network-capable socket
+	portNumber = atoi(argv[1]); 								// Convert port to an integer from a string
+	serverAddress.sin_family = AF_INET; 				// Create a network-capable socket
 	serverAddress.sin_port = htons(portNumber); // Store the port number
 	serverAddress.sin_addr.s_addr = INADDR_ANY; // Any address is allowed for connection to this process
 
@@ -29,26 +47,49 @@ int main(int argc, char *argv[])
 	if (listenSocketFD < 0) error("ERROR opening socket");
 
 	// Enable the socket to begin listening
-	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) // Connect socket to port
+	// Connect socket to port
+	if (bind(listenSocketFD, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) < 0) {
 		error("ERROR on binding");
+	}
 	listen(listenSocketFD, 5); // Flip the socket on - it can now receive up to 5 connections
 
-	// Accept a connection, blocking if one is not available until one connects
-	sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect, clientAddress will hold client data
-	establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
-	if (establishedConnectionFD < 0)
-		error("ERROR on accept");
+	// Loop indefinitely
+	while(1) {
+		// Accept a connection, blocking if one is not available until one connects
+		sizeOfClientInfo = sizeof(clientAddress); // Get the size of the address for the client that will connect, clientAddress will hold client data
+		establishedConnectionFD = accept(listenSocketFD, (struct sockaddr *)&clientAddress, &sizeOfClientInfo); // Accept
+		if (establishedConnectionFD < 0) {
+			error("ERROR on accept");
+		}
 
-	// Get the message from the client and display it
-	memset(buffer, '\0', 256);
-	charsRead = recv(establishedConnectionFD, buffer, 255, 0); // Read the client's message from the socket
-	if (charsRead < 0) error("ERROR reading from socket");
-	printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+		spawnPid = fork();
 
-	// Send a Success message back to the client
-	charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
-	if (charsRead < 0) error("ERROR writing to socket");
-	close(establishedConnectionFD); // Close the existing socket which is connected to the client
+		switch(spawnPid) {
+			case -1:
+				error("Hull Breach");
+				break;
+
+			case 0:
+				// Get the message from the client and display it
+				memset(buffer, '\0', 256);
+				charsRead = recv(establishedConnectionFD, buffer, 255, 0); // Read the client's message from the socket
+				if (charsRead < 0) error("ERROR reading from socket");
+				printf("SERVER: I received this from the client: \"%s\"\n", buffer);
+
+				// Send a Success message back to the client
+				charsRead = send(establishedConnectionFD, "I am the server, and I got your message", 39, 0); // Send success back
+				if (charsRead < 0) {
+					error("ERROR writing to socket");
+				}
+				close(establishedConnectionFD); // Close the existing socket which is connected to the client
+				break;
+
+			default:
+				spawnPid = waitpid(-1, &sp_child_exit, WNOHANG);
+
+				break;
+		}
+	}
 	close(listenSocketFD); // Close the listening socket
 	return 0;
 }
